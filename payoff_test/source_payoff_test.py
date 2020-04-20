8dac77afdbd03fb0aa7a1d21fb4476cd00cd6c21
Julien <julien.caselmann@tum.de>
2020-04-20 11:42:53 +0200

    Code improvement for future payoff functions

--- payoff_test/source_payoff_test.py
+++ payoff_test/source_payoff_test.py
@@ -13,7 +13,7 @@ def u_con (omega, theta, i, epsilon):
     bias_epsilon = 0.0
     return (theta-bias_theta)*(epsilon-bias_epsilon)*(omega+bias_omega)-omega*(1-epsilon)*theta*i
 
-def new_u(omega, theta, i, epsilon):
+def single_u(omega, theta, i, epsilon):
     return epsilon*theta*i*(1-omega)
 
 def apply(samples, fun):
@@ -23,6 +23,14 @@ def apply(samples, fun):
     epsilon = samples[3]
     return fun(omega, theta, i , epsilon)
 
+def fun_to_string(fun):
+    if fun == u_pro:
+        return '$u_{pro}$'
+    elif fun == u_con:
+        return '$u_{con}$'
+    elif fun == single_u:
+        return '$single_{u}$'
+
 seed(1)
 
 def buildRandomSamples():
@@ -31,24 +39,25 @@ def buildRandomSamples():
         samples.append([random.uniform(0,1/1000), random.uniform(0,1), random.uniform(0,1), random.uniform(0.7, 1)])
     return samples
 
-def compute(samples):
+def compute(samples, fun_pro, fun_con):
     results = []
     for i in range(len(samples)):
-        results.append([i, round(samples[i][0], 6), round(samples[i][1], 6), round(samples[i][2], 6), round(samples[i][3], 6), round(apply(samples[i], u_con), 6), round(apply(samples[i], u_pro), 6)])
-    return results
+        results.append([i, round(samples[i][0], 6), round(samples[i][1], 6), round(samples[i][2], 6), round(samples[i][3], 6), round(apply(samples[i], fun_pro), 6), round(apply(samples[i], fun_con), 6)])
+    return (fun_to_string(fun_pro), fun_to_string(fun_con), results)
 
-def outputToLatex(results):
-    txt = '\\begin{table}\n\\begin{tabular}{c|c|c|c|c|c|c}\n\\# & $\\omega$ & $\\theta$ & $I$ & $\\epsilon$ & $u_{con}$ & $u_{pro}$' + "\\" + "\\\n" + '\\hline\n'
+def outputToLatex(res):
+    txt = '\\begin{table}\n\\caption{Pro:' + res[0] + ', Con:' + res[1] + '}\n\\begin{tabular}{c|c|c|c|c|c|c}\n\\# & $\\omega$ & $\\theta$ & $I$ & $\\epsilon$ & Pro & Con' + "\\" + "\\\n" + '\\hline\n'
     counter = 0
+    results = res[2]
     for i in range(len(results)):
         txt += str(results[i][0]) + ' & ' + str(results[i][1]) + ' & ' + str(results[i][2]) + ' & ' + str(results[i][3]) +  ' & ' + str(results[i][4]) + ' & ' + str(results[i][5]) + ' & ' + str(results[i][6]) + '\\\\\n'
         if i == counter + 50:
             counter = i
-            txt += '\\end{tabular}\n\\end{table}\n\\newpage\n\\begin{table}\n\\begin{tabular}{c|c|c|c|c|c|c}\n\\# & $\\omega$ & $\\theta$ & $I$ & $\\epsilon$ & $u_{con}$ & $u_{pro}$' + "\\" + "\\\n" + '\\hline\n'
+            txt += '\\end{tabular}\n\\end{table}\n\\newpage\n\\begin{table}\n\\begin{tabular}{c|c|c|c|c|c|c}\n\\# & $\\omega$ & $\\theta$ & $I$ & $\\epsilon$ & Pro & Con' + "\\" + "\\\n" + '\\hline\n'
     return (txt + '\\end{tabular}\n\\end{table}')
 
 sam = buildRandomSamples()
-res= compute(sam)
+res= compute(sam, u_pro, u_con)
 
 with open('table_2_payoff_test.tex', 'w') as file:
     file.write(outputToLatex(res))
