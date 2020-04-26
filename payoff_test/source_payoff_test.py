1581d5bbb1aa6d1b0e32db5d0b29e68751ab1c8b
Julien <julien.caselmann@tum.de>
2020-04-26 13:49:50 +0200

    Huge improvement on model. Adapted payoff functions to remarks from Mueller on meeting 4. Now they are simpler AND not parallel AND compute realistic values!

--- payoff_test/source_payoff_test.py
+++ payoff_test/source_payoff_test.py
@@ -2,6 +2,8 @@ from random import seed
 import random
 import math
 
+#######################################################################################################
+# first try to find payoff function (20 - 26 April 2020)
 def sig(x):
     return (1/(1+math.exp(-x)))
 
@@ -54,12 +56,31 @@ def counter_u_con(omega, theta, i, epsilon):
     
     return 0.2 * i_payoff + 3.2 * omega_payoff + 0.28 * theta_payoff + 0.32 * epsilon_payoff
 
+#######################################################################################################
+# second try to find payoff function (27 - 30 April 2020)
+
+def u_con_mark2(omega, theta, i, epsilon, X, p_meet_Z):
+    return (X + p_meet_Z)*(theta + i) - (2 - X - p_meet_Z)*(1-epsilon+100000*omega)
+
+def u_pro_mark2(omega, theta, i, epsilon, Y, p_meet_Z):
+    return (2-Y-p_meet_Z) * (theta + i) - (Y + p_meet_Z) * (1-epsilon + 100000*omega)
+
+#######################################################################################################
 def apply(samples, fun):
     omega = samples[0]
     theta = samples[1]
     i = samples[2]
     epsilon = samples[3]
-    return fun(omega, theta, i , epsilon)
+    if fun == u_con_mark2:
+        X = samples[4]
+        p_meet_Z1 = samples [5]
+        return fun(omega, theta, i, epsilon, X, p_meet_Z1)
+    elif fun == u_pro_mark2:
+        Y = 1-samples[4]
+        p_meet_Z0 = samples[6]
+        return fun(omega, theta, i, epsilon, Y, p_meet_Z0)
+    else:
+        return fun(omega, theta, i , epsilon)
 
 def fun_to_string(fun):
     if fun == u_pro:
@@ -72,39 +93,66 @@ def fun_to_string(fun):
         return '$counter_{u_{con}}$'
     elif fun == counter_u_pro:
         return '$counter_{u_{pro}}$'
+    elif fun == u_con_mark2:
+        return '$u_{con} (mark 2)$'
+    elif fun == u_pro_mark2:
+        return '$u_{pro} (mark 2)$'
 
 seed(1)
 
-def buildRandomSamples():
+def buildRandomSamples(mark):
     samples = []
-    #vaccine risk ist maximal 1/1000, vaccine efficacity ist mind. 75% sonst nicht zugelassen
-    for i in range(1000):
-        samples.append([random.uniform(0, 1/100000), random.uniform(0,1), random.uniform(0,1), random.uniform(0.75, 1)])
+    if mark == 2:
+        for i in range(1000):
+            samples.append([random.uniform(0, 1/100000), random.uniform(0,1), random.uniform(0,1), random.uniform(0.75, 1), random.uniform(0,1), random.uniform(0,1), random.uniform(0,1)])
+    else:
+        #vaccine risk ist maximal 1/1000, vaccine efficacity ist mind. 75% sonst nicht zugelassen
+        for i in range(1000):
+            samples.append([random.uniform(0, 1/100000), random.uniform(0,1), random.uniform(0,1), random.uniform(0.75, 1)])
+    
     return samples
 
 def compute(samples, fun_pro, fun_con, app_sigmoid):
     results = []
-    for i in range(len(samples)):
-        if app_sigmoid:
-            results.append([i, round(samples[i][0], 6), round(samples[i][1], 6), round(samples[i][2], 6), round(samples[i][3], 6), round(sig(-apply(samples[i], fun_pro)), 6), round(sig(apply(samples[i], fun_con)), 6)])
-        else:
-            results.append([i, round(samples[i][0], 6), round(samples[i][1], 6), round(samples[i][2], 6), round(samples[i][3], 6), round(apply(samples[i], fun_pro), 6), round(apply(samples[i], fun_con), 6)])
+    if fun_con == u_con_mark2:
+        for i in range(len(samples)):
+            if app_sigmoid:
+                results.append([i, round(samples[i][0], 6), round(samples[i][1], 6), round(samples[i][2], 6), round(samples[i][3], 6), round(samples[i][4], 6), round(samples[i][5], 6), round(samples[i][6], 6), round(sig(-apply(samples[i], fun_pro)), 6), round(sig(apply(samples[i], fun_con)), 6)])
+            else:
+                results.append([i, round(samples[i][0], 6), round(samples[i][1], 6), round(samples[i][2], 6), round(samples[i][3], 6), round(samples[i][4], 6), round(samples[i][5], 6), round(samples[i][6], 6), round(apply(samples[i], fun_pro), 6), round(apply(samples[i], fun_con), 6)])
+    else:
+        for i in range(len(samples)):
+            if app_sigmoid:
+                results.append([i, round(samples[i][0], 6), round(samples[i][1], 6), round(samples[i][2], 6), round(samples[i][3], 6), round(sig(-apply(samples[i], fun_pro)), 6), round(sig(apply(samples[i], fun_con)), 6)])
+            else:
+                results.append([i, round(samples[i][0], 6), round(samples[i][1], 6), round(samples[i][2], 6), round(samples[i][3], 6), round(apply(samples[i], fun_pro), 6), round(apply(samples[i], fun_con), 6)])
+    
     return (fun_to_string(fun_pro), fun_to_string(fun_con), str(app_sigmoid), results)
 
 def outputToLatex(res):
-    txt = '\\begin{table}\n\\caption{Pro:' + res[0] + ', Con:' + res[1] + '$\\mathrm{sig}:' + res[2] + '$}\n\\begin{tabular}{c|c|c|c|c|c|c}\n\\# & $\\omega$ & $\\theta$ & $I$ & $\\epsilon$ & Pro & Con' + "\\" + "\\\n" + '\\hline\n'
+    mark2 = res[1] == fun_to_string(u_con_mark2)
+    if mark2:
+        txt = '\\begin{table}\n\\caption{Pro:' + res[0] + ', Con:' + res[1] + '$\\mathrm{sig}:' + res[2] + '$}\n\\begin{tabular}{c|c|c|c|c|c|c|c|c|c}\n\\# & $\\omega$ & $\\theta$ & $I$ & $\\epsilon$ & $X_t$ & $p_{meetZ_1}$ & $p_{meetZ_0}$ & Pro & Con' + "\\" + "\\\n" + '\\hline\n'
+    else:
+        txt = '\\begin{table}\n\\caption{Pro:' + res[0] + ', Con:' + res[1] + '$\\mathrm{sig}:' + res[2] + '$}\n\\begin{tabular}{c|c|c|c|c|c|c}\n\\# & $\\omega$ & $\\theta$ & $I$ & $\\epsilon$ & Pro & Con' + "\\" + "\\\n" + '\\hline\n'
     counter = 0
     results = res[3]
     for i in range(len(results)):
-        txt += str(results[i][0]) + ' & ' + str(results[i][1]) + ' & ' + str(results[i][2]) + ' & ' + str(results[i][3]) +  ' & ' + str(results[i][4]) + ' & ' + str(results[i][5]) + ' & ' + str(results[i][6]) + '\\\\\n'
+        if mark2:
+            txt += str(results[i][0]) + ' & ' + str(results[i][1]) + ' & ' + str(results[i][2]) + ' & ' + str(results[i][3]) +  ' & ' + str(results[i][4]) + ' & ' + str(results[i][5]) + ' & ' + str(results[i][6]) + '&' + str(results[i][7]) + '&' + str(results[i][8]) + '&' + str(results[i][9]) + '\\\\\n'
+        else:
+            txt += str(results[i][0]) + ' & ' + str(results[i][1]) + ' & ' + str(results[i][2]) + ' & ' + str(results[i][3]) +  ' & ' + str(results[i][4]) + ' & ' + str(results[i][5]) + ' & ' + str(results[i][6]) + '\\\\\n'
         if i == counter + 45:
             counter = i
-            txt += '\\end{tabular}\n\\end{table}\n\\newpage\n\\begin{table}\n\\begin{tabular}{c|c|c|c|c|c|c}\n\\# & $\\omega$ & $\\theta$ & $I$ & $\\epsilon$ & Pro & Con' + "\\" + "\\\n" + '\\hline\n'
+            if mark2:
+                txt += '\\end{tabular}\n\\end{table}\n\\newpage\n\\begin{table}\n\\begin{tabular}{c|c|c|c|c|c|c|c|c|c}\n\\# & $\\omega$ & $\\theta$ & $I$ & $\\epsilon$ & $X_t$ & $p_{meetZ_1}$ & $p_{meetZ_0}$ & Pro & Con' + "\\" + "\\\n" + '\\hline\n'
+            else:
+                txt += '\\end{tabular}\n\\end{table}\n\\newpage\n\\begin{table}\n\\begin{tabular}{c|c|c|c|c|c|c}\n\\# & $\\omega$ & $\\theta$ & $I$ & $\\epsilon$ & Pro & Con' + "\\" + "\\\n" + '\\hline\n'
     return (txt + '\\end{tabular}\n\\end{table}')
 
-sam = buildRandomSamples()
-sigmoid = False
-res = compute(sam, counter_u_pro, counter_u_con, sigmoid)
+sam = buildRandomSamples(2)
+sigmoid = True
+res = compute(sam, u_pro_mark2, u_con_mark2, sigmoid)
 
-with open('table_2_payoff_test.tex', 'w') as file:
+with open('table_3_payoff_test.tex', 'w+') as file:
     file.write(outputToLatex(res))
