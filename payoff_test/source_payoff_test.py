1a036a0b7601f90b1427748b711804f1dbd23d2a
Julien <julien.caselmann@tum.de>
2020-04-30 17:50:28 +0200

    implemented new modeling idea from meeting 5. many TODOs, since it's values are trash

--- payoff_test/source_payoff_test.py
+++ payoff_test/source_payoff_test.py
@@ -1,6 +1,8 @@
 from random import seed
 import random
 import math
+import numpy as np
+import matplotlib.pyplot as plt
 
 #######################################################################################################
 # first try to find payoff function (20 - 26 April 2020)
@@ -59,28 +61,23 @@ def counter_u_con(omega, theta, i, epsilon):
 #######################################################################################################
 # second try to find payoff function (27 - 30 April 2020)
 
-def u_con_mark2(omega, theta, i, epsilon, X, p_meet_Z):
-    return (X + p_meet_Z)*(theta + i) - (2 - X - p_meet_Z)*(1-epsilon+100000*omega)
+def u_con_mark2(omega, theta, i, epsilon, kappa, X, p_meet_Z1, p_meet_Z0):
+    return (X + p_meet_Z1)*(theta + i) - (2 - X - p_meet_Z1)*(1-epsilon+kappa*omega)
 
-def u_pro_mark2(omega, theta, i, epsilon, Y, p_meet_Z):
-    return (2-Y-p_meet_Z) * (theta + i) - (Y + p_meet_Z) * (1-epsilon + 100000*omega)
+def u_pro_mark2(omega, theta, i, epsilon, kappa, X, p_meet_Z1, p_meet_Z0):
+    return (2-1+X-p_meet_Z0) * (theta + i) - (1-X + p_meet_Z0) * (1-epsilon + kappa*omega)
+
 
 #######################################################################################################
-def apply(samples, fun):
-    omega = samples[0]
-    theta = samples[1]
-    i = samples[2]
-    epsilon = samples[3]
-    if fun == u_con_mark2:
-        X = samples[4]
-        p_meet_Z1 = samples [5]
-        return fun(omega, theta, i, epsilon, X, p_meet_Z1)
-    elif fun == u_pro_mark2:
-        Y = 1-samples[4]
-        p_meet_Z0 = samples[6]
-        return fun(omega, theta, i, epsilon, Y, p_meet_Z0)
-    else:
-        return fun(omega, theta, i , epsilon)
+# third try (31 April - 7 May 2020) this time: payoff included in transition rates!
+
+def transition_pro(omega, theta, i, epsilon, kappa, N, X, N_X, N_Y, theta_X, theta_Y, mu):
+    return (mu*(N-X)*((theta_X * (X + N_X + theta + i))/(theta_X*(X+N_X+theta+i)+(N-X+N_Y + 1-epsilon+kappa*omega))))
+
+def transition_con(omega, theta, i, epsilon, kappa, N, X, N_X, N_Y, theta_X, theta_Y, mu):
+    return (mu*X * ((theta_Y * (N-X+N_Y + 1-epsilon+kappa*omega))/((X+N_X+theta+i) + theta_Y * (N-X+N_Y+1-epsilon+kappa*omega))))
+
+#######################################################################################################
 
 def fun_to_string(fun):
     if fun == u_pro:
@@ -97,62 +94,191 @@ def fun_to_string(fun):
         return '$u_{con} (mark 2)$'
     elif fun == u_pro_mark2:
         return '$u_{pro} (mark 2)$'
-
-seed(1)
-
-def buildRandomSamples(mark):
-    samples = []
-    if mark == 2:
-        for i in range(1000):
-            samples.append([random.uniform(0, 1/100000), random.uniform(0,1), random.uniform(0,1), random.uniform(0.75, 1), random.uniform(0,1), random.uniform(0,1), random.uniform(0,1)])
+    elif fun == transition_con:
+        return 'rate for $X_t \\rightarrow X_t - 1$'
+    elif fun == transition_pro:
+        return 'rate for $X_t \\rightarrow X_t + 1$'
     else:
-        #vaccine risk ist maximal 1/1000, vaccine efficacity ist mind. 75% sonst nicht zugelassen
-        for i in range(1000):
-            samples.append([random.uniform(0, 1/100000), random.uniform(0,1), random.uniform(0,1), random.uniform(0.75, 1)])
-    
+        return 'You need to define a string for this function'
+
+def apply(samples, fun_pro, fun_con, **kwargs):
+    args = kwargs.get('args',[])
+    arg_list = samples
+    for arg in args:
+        arg_list.append(args[arg])
+    res_pro = fun_pro(*arg_list)
+    res_con = fun_con(*arg_list)
+    return (res_pro,res_con)
+
+def buildRandomSamples(numberOfSamples, kappa, allDynamic, maxElementsPerPage):
+    samples = []
+    if allDynamic:
+        for j in range(numberOfSamples):
+            omega = random.uniform(0, 1/kappa)
+            theta = random.uniform(0, 1)
+            i = random.uniform(0, 1)
+            epsilon = random.uniform(0.75,1)
+            samples.append([omega,theta,i,epsilon])
+    else:
+        omega = random.uniform(0, 1/kappa)
+        theta = random.uniform(0, 1)
+        epsilon = random.uniform(0.75,1)
+        counter = 0
+        for j in range(numberOfSamples):
+            i = random.uniform(0, 1)
+            samples.append([omega,theta,i,epsilon])
+            if j == counter + maxElementsPerPage:
+                counter = j
+                omega = random.uniform(0, 1/kappa)
+                theta = random.uniform(0, 1)
+                epsilon = random.uniform(0.75,1)
     return samples
 
-def compute(samples, fun_pro, fun_con, app_sigmoid):
+def buildRandomArguments(fun,kappa,**kwargs):
+    args = {'\\kappa' : kappa}
+    if fun == u_pro_mark2:
+        args['X_t'] = round(random.uniform(0,1),6)
+        args['p_{meetZ_1}'] = round(random.uniform(0,1),6)
+        args['p_{meetZ_0}'] = round(random.uniform(0,1),6)
+    elif fun == transition_pro:
+        N = kwargs.get('N',0)
+        max_zel_X = kwargs.get('maxZelX',0)
+        max_zel_Y = kwargs.get('maxZelY',0)
+        args['N'] = N
+        args['X_t'] = random.randint(0,N)
+        args['N_X']  = random.randint(0,max_Zel_X)
+        args['N_Y'] = random.randint(0,max_Zel_Y)
+        args['\\theta_X'] = round(random.uniform(0,1),6)
+        args['\\theta_Y'] = round(random.uniform(0,1),6)
+        args['\\mu'] = round(random.uniform(0,1),6)
+    return args
+
+def compute(samples,arguments, fun_pro, fun_con, **kwargs):
     results = []
-    if fun_con == u_con_mark2:
-        for i in range(len(samples)):
-            if app_sigmoid:
-                results.append([i, round(samples[i][0], 6), round(samples[i][1], 6), round(samples[i][2], 6), round(samples[i][3], 6), round(samples[i][4], 6), round(samples[i][5], 6), round(samples[i][6], 6), round(sig(-apply(samples[i], fun_pro)), 6), round(sig(apply(samples[i], fun_con)), 6)])
-            else:
-                results.append([i, round(samples[i][0], 6), round(samples[i][1], 6), round(samples[i][2], 6), round(samples[i][3], 6), round(samples[i][4], 6), round(samples[i][5], 6), round(samples[i][6], 6), round(apply(samples[i], fun_pro), 6), round(apply(samples[i], fun_con), 6)])
-    else:
-        for i in range(len(samples)):
-            if app_sigmoid:
-                results.append([i, round(samples[i][0], 6), round(samples[i][1], 6), round(samples[i][2], 6), round(samples[i][3], 6), round(sig(-apply(samples[i], fun_pro)), 6), round(sig(apply(samples[i], fun_con)), 6)])
-            else:
-                results.append([i, round(samples[i][0], 6), round(samples[i][1], 6), round(samples[i][2], 6), round(samples[i][3], 6), round(apply(samples[i], fun_pro), 6), round(apply(samples[i], fun_con), 6)])
+    app_sigmoid = kwargs.get('sig',False)
+
+    for i in range(len(samples)):
+        current_res = [i]
+        #all sampled parameters needed for computation, rounded to 6 digits
+        for j in range(len(samples[i])):
+            current_res.append(round(samples[i][j],6))
+        #the two computation results (pro and con)
+        res_pro,res_con = apply(samples[i], fun_pro, fun_con,args = arguments)
+
+        #do we want the sigmoid function to transform the results?
+        if app_sigmoid:
+            current_res.append(round(sig(-res_pro),6))
+            current_res.append(round(sig(res_con),6))
+        else:
+            current_res.append(round(res_pro,6))
+            current_res.append(round(res_con,6))
+        #push everything in results
+        results.append(current_res)
     
     return (fun_to_string(fun_pro), fun_to_string(fun_con), str(app_sigmoid), results)
 
-def outputToLatex(res):
-    mark2 = res[1] == fun_to_string(u_con_mark2)
-    if mark2:
-        txt = '\\begin{table}\n\\caption{Pro:' + res[0] + ', Con:' + res[1] + '$\\mathrm{sig}:' + res[2] + '$}\n\\begin{tabular}{c|c|c|c|c|c|c|c|c|c}\n\\# & $\\omega$ & $\\theta$ & $I$ & $\\epsilon$ & $X_t$ & $p_{meetZ_1}$ & $p_{meetZ_0}$ & Pro & Con' + "\\" + "\\\n" + '\\hline\n'
+def getLatexIntro(args, skipKappa):
+    intro = 'Following fixed values are considered:\n\\begin{itemize}\n'
+    for itemName in args:
+        if not (itemName == '\\kappa' and skipKappa):
+           intro += '\\item $' + itemName + ' = ' + str(args[itemName]) + '$\n'
+    intro += '\\end{itemize}\n'
+    return intro
+
+def getLatexSampleExplanation(counter,allDynamic):
+    if allDynamic:
+        expl = 'Following values are completely dynamic and (randomly) generated every step:\n\\begin{itemize}\n'
+        expl += '\\item $\\omega$\n\\item $\\theta$\n\\item $\\epsilon$\n\\item $I$\n\\end{itemize}\n'
     else:
-        txt = '\\begin{table}\n\\caption{Pro:' + res[0] + ', Con:' + res[1] + '$\\mathrm{sig}:' + res[2] + '$}\n\\begin{tabular}{c|c|c|c|c|c|c}\n\\# & $\\omega$ & $\\theta$ & $I$ & $\\epsilon$ & Pro & Con' + "\\" + "\\\n" + '\\hline\n'
-    counter = 0
+        expl = 'Following values are newly (randomly) generated all ' + str(counter) + ' steps:\n\\begin{itemize}\n'
+        expl += '\\item $\\omega$\n\\item $\\theta$\n\\item $\\epsilon$\n\\end{itemize}\nThe value $I$ is completely dynamic and determined randomly at every step.'
+    return expl
+
+def getLatexCaption(res):
+    fun_pro = res[0]
+    fun_con = res[1]
+    sig = res[2]
     results = res[3]
-    for i in range(len(results)):
-        if mark2:
-            txt += str(results[i][0]) + ' & ' + str(results[i][1]) + ' & ' + str(results[i][2]) + ' & ' + str(results[i][3]) +  ' & ' + str(results[i][4]) + ' & ' + str(results[i][5]) + ' & ' + str(results[i][6]) + '&' + str(results[i][7]) + '&' + str(results[i][8]) + '&' + str(results[i][9]) + '\\\\\n'
+    if fun_pro == fun_to_string(transition_pro):
+        caption = '\\begin{table}\n\\caption{Pro: ' + fun_pro + ', Con: ' + fun_con + '}\n\\begin{tabular*}{\\linewidth}{'
+    else:
+        caption = '\\begin{table}\n\\caption{Pro: ' + fun_pro + ', Con: ' + fun_con + ', $\\mathrm{sigmoid}: ' + sig + '$}\n\\begin{tabular*}{\\linewidth}{'
+    for i in range(len(results[0])):
+        if i == len(results[0]) - 1:
+            caption += 'c}\n'
         else:
-            txt += str(results[i][0]) + ' & ' + str(results[i][1]) + ' & ' + str(results[i][2]) + ' & ' + str(results[i][3]) +  ' & ' + str(results[i][4]) + ' & ' + str(results[i][5]) + ' & ' + str(results[i][6]) + '\\\\\n'
-        if i == counter + 45:
+            caption += 'c|'
+    caption += '\\# & $\\omega$ & $\\theta$ & $I$ & $\\epsilon$ & Pro & Con \\\\\n\\hline\n'
+    return caption
+
+def getLatexLine(results):
+    line = ''
+    for i in range(len(results)):
+        if i == len(results) - 1:
+            line += str(results[i]) + '\\\\\n'
+        else:
+            line += str(results[i]) + ' & '
+    return line
+
+def outputToLatex(res,args,skipKappa,maxElementsPerPage,allDynamic):
+    txt = ''
+    txt += getLatexIntro(args, skipKappa)
+    expl = getLatexSampleExplanation(maxElementsPerPage,allDynamic)
+    txt += expl
+    caption = getLatexCaption(res)
+    txt += caption
+    results = res[3]
+    counter = 0
+    for i in range(len(results)):
+        txt += getLatexLine(results[i])
+        if i == counter + maxElementsPerPage:
             counter = i
-            if mark2:
-                txt += '\\end{tabular}\n\\end{table}\n\\newpage\n\\begin{table}\n\\begin{tabular}{c|c|c|c|c|c|c|c|c|c}\n\\# & $\\omega$ & $\\theta$ & $I$ & $\\epsilon$ & $X_t$ & $p_{meetZ_1}$ & $p_{meetZ_0}$ & Pro & Con' + "\\" + "\\\n" + '\\hline\n'
-            else:
-                txt += '\\end{tabular}\n\\end{table}\n\\newpage\n\\begin{table}\n\\begin{tabular}{c|c|c|c|c|c|c}\n\\# & $\\omega$ & $\\theta$ & $I$ & $\\epsilon$ & Pro & Con' + "\\" + "\\\n" + '\\hline\n'
-    return (txt + '\\end{tabular}\n\\end{table}')
+            txt += '\\end{tabular*}\n\\end{table}\n\\newpage\n' + caption
 
-sam = buildRandomSamples(2)
-sigmoid = True
-res = compute(sam, u_pro_mark2, u_con_mark2, sigmoid)
+    return (txt + '\\end{tabular*}\n\\end{table}')
 
-with open('table_3_payoff_test.tex', 'w+') as file:
-    file.write(outputToLatex(res))
+
+#######################################################################################################
+# set all parameters here
+kappa = 100000
+pop = 1000000
+max_Zel_X = 0.15 * pop
+max_Zel_Y = 0.05 * pop
+maxElementsPerPage = 45
+numberOfSamples = 1000
+allDynamic = False
+skipKappa = False
+
+# reminder: if you are using transition_pro and transition_con, useSigmoid should always be set to False
+#           (True makes no sense and: you get a math overflow error lel)
+useSigmoid = False
+
+fun_pro = transition_pro
+fun_con = transition_con
+
+#name of the Latex file (in this directory) you are writing:
+fileName = 'table_5_payoff_test.tex'
+
+# leave commented if you want unrepeated random numbers, uncomment if you want to have the same numbers every compilation
+#seed(1)
+
+
+
+
+
+
+
+
+#######################################################################################################
+# do not touch!
+
+samples = buildRandomSamples(numberOfSamples,kappa, allDynamic, maxElementsPerPage)
+if fun_pro == transition_pro:
+    args = buildRandomArguments(transition_pro,kappa,N=pop,maxZelX = max_Zel_X, maxZelY = max_Zel_Y)
+elif fun_pro == u_pro_mark2:
+    args = buildRandomArguments(u_pro_mark2,kappa)
+
+res = compute(samples,args, fun_pro, fun_con, sig=useSigmoid)
+
+with open(fileName, 'w+') as file:
+    file.write(outputToLatex(res,args,skipKappa,maxElementsPerPage,allDynamic))
