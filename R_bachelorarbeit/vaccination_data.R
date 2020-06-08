23771523723d5169312ddc2e0e43248f74fccca7
Julien <julien.caselmann@tum.de>
2020-06-08 16:05:19 +0200

    R shit

---
+++ R_bachelorarbeit/vaccination_data.R
@@ -0,0 +1,4 @@
+
+vaccination_data = read.csv2('C:/Users/ge69fup/Documents/Uni/TUM/Mathe_B_Sc/SS_20/Bachelorarbeit/bachelorarbeit-repo/impfdaten_bachelorarbeit/merged_impfdaten.csv', header=TRUE)
+
+hist(vaccination_data$Anzahl)
