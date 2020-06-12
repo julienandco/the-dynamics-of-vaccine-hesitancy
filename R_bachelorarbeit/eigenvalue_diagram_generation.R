8db87e798078f8051f131a45d5fb4043e268b50c
Julien <julien.caselmann@tum.de>
2020-06-12 12:25:34 +0200

    improved R script for eigenval diagram2

--- R_bachelorarbeit/eigenvalue_diagram_generation.R
+++ R_bachelorarbeit/eigenvalue_diagram_generation.R
@@ -2,39 +2,6 @@ setwd("C:/Users/ge69fup/Documents/Uni/TUM/Mathe_B_Sc/SS_20/Bachelorarbeit/bachel
 #setwd("D:/Dokumente/Uni/TUM/Mathe_B_Sc/SS_20/Bachelorarbeit/bachelorarbeit-repo/R_bachelorarbeit");
 
 
-# parameter
-bbeta = 5    # contact rate
-alpha = 0.45  # recovery rate
-B     = 2    # birthrate - deterimes thetime scale -- fixed.
-
-theta1 = 1; # reinforcement parameter of vacc pro (x)
-theta2 = theta1; # reinforcement parameter of vacc con (1-x)
-n1     = 10;    # zelots pro vacc
-n2     = 10;    # zealots contra vacc
-a      = 0.1;    # influence I
-Omega  = 0.0;    # influence vaccination oponent
-c      = 200# 1;   # time scale reinforce
-
-# adapt n1, n2 s.t. a*i+n1 = a*(1-i)+n2
-# choose n1 minial, s.t. we have a non-negative n2 (=0)
-adapt_params <- function(a,i.star, givenN1, givenN2, givenTheta1){
-  if(missing(givenN1)){
-    givenN1 = max(c(a*(1-2*i.star), 0));
-  }
-  if(missing(givenN2)){
-    givenN2 = a*(2*i.star-1)+givenN1;
-  }
-  
-  n.star = a*i.star+givenN1
-  
-  if(missing(givenTheta1)){
-    givenTheta1 = (1-2*n.star)/(1+2*n.star);
-  }
-  
-  givenTheta2 = givenTheta1
-
-  return(c(givenN1,givenN2,givenTheta1,givenTheta2))
-}
 "
 #my system (no params neded)
 rhs <- function(state){
@@ -91,10 +58,10 @@ get.jacobian <- function(state,params){
   return(J);
 }
 
-check_for_imaginary_ev <- function(evals){
+check_for_imaginary_ev <- function(evals, tol){
   t = 1;
   while(t <= length(evals)){
-    if(Re(evals[t]) == 0 && Im(evals[t]) != 0){
+    if(abs(Re(evals[t])) <= tol && Im(evals[t]) != 0){
       return (TRUE);
     }
     t = t+1;
@@ -130,41 +97,65 @@ s  = 0.8;
 i = 0.2;
 x = 0.2;
 
+# parameter
+bbeta = 5    # contact rate 5
+alpha = 0.45  # recovery rate 0.45
+B     = 2    # birthrate - deterimes thetime scale -- fixed. 2
+
+theta1 = 1; # reinforcement parameter of vacc pro (x)
+theta2 = theta1; # reinforcement parameter of vacc con (1-x)
+n1     = 10;    # zelots pro vacc
+n2     = 10;    # zealots contra vacc
+a      = 0.1;    # influence I: 0.1
+c      = 0.3# 1;   # time scale reinforce
+
+# we calibrate the system s.t. x=1/2 is a stat states
+s.star = (B+alpha)/bbeta;
+i.star = (1-0.5)*B/(alpha+B)-B/bbeta;
+cat("i.star = ", i.star, "\n");
+
+n1 = max(c(a*(1-2*i.star), 0));
+n2 = a*(2*i.star-1)+n1;
+n.star = a*i.star+n1
+theta1 = (1-2*n.star)/(1+2*n.star);
+theta2 = theta1;
 
 #alpha, beta, mu, a, b, nx, ny, thetax, thetay
 params_names = c('alpha', 'bbeta', 'c', 'a', 'B', 'n1', 'n2', 'theta1', 'theta2');
 params = c(alpha,bbeta,c,a,B,n1,n2,theta1,theta2);
 
 #my_fav_param = c(index, value); (index in params)
-#für B gehts am meisten ab, aber trotzdem kein iomega...
-#n1 ist knapp daneben
-my_fav_param = c(9,0);
+
+my_fav_param = c(5,0);
 
 params[my_fav_param[1]] = my_fav_param[2];
 
 state = c(s,i,x);
-horizon = 5;
+horizon = 100;
 
 h = 0.01; tt = 0;
 #start
 J = get.jacobian(state,params)
 evals = eigen(J)$values;
 title = paste('Spektrum in Abhängigkeit von',params_names[my_fav_param[1]]);
-plot(main=title,Re(evals), Im(evals), xlab = 'Re', ylab = 'Im', cex = 0.01);
+#plot(main=title,Re(evals), Im(evals), xlab = 'Re', ylab = 'Im', cex = 0.01);
 #res <<- numeric();
 res <<- data.frame('tt', params_names[my_fav_param[1]], 's*','i*','x*','eig_val_1','eig_val_2','eig_val_3');
-
+plot(main=title,0,0,xlab='Re',ylab='Im',cex=0.01);
 tol = 1e-6;
 max_iter = 500000;
 
 while(tt < horizon){
   cat('tt', tt);
   steady = get.stat.state(state, tol, max_iter,params);
-  J = get.jacobian(steady,params)
+  #todo: wie bekomme ich diese Translation durch Parameter hin?
+  #J = get.jacobian(steady,params) + 0.25*diag(3);
+  
+  J = get.jacobian(steady,params);
   evals = eigen(J)$values;
   points(Re(evals), Im(evals), cex = 0.01);
   
-  if(check_for_imaginary_ev(evals)){
+  if(check_for_imaginary_ev(evals,tol)){
     cat('Purely imaginary eigenvalue at steady state = ', steady, 'tt=', tt);
     cat('Re:', Re(evals));
     cat('Im:', Im(evals));
---
+++ R_bachelorarbeit/readme.txt
@@ -0,0 +1,16 @@
+Wahl von my_fav_param:
+
+#alpha gibt nur reelle EV
+#bbeta gibt nur reelle EV
+#c bleibt sehr nah an reeller achse (sieht aus wie alpha und beta, hat nur einen im.wert bei -1.6+- 0.6i ugf)
+#a gibt nur reelle EV
+#für B bekomme ich schönes blatt, aber trotzdem kein iomega...
+#n1 hat ab Re >= -1.0 nur noch reelle EV
+#n2 im bereich der y-achse nur noch reelle EV
+#theta1 konvergiert nicht mehr ab tt = 1.74, davor nur reell
+#theta2 konvergiert nicht mehr ab tt = 1.22, davor nur reell
+
+
+Wählen also B, da schönes Blatt. Wenn wir aber andere Parameter zu Beginn ändern und danach mit B iterieren:
+#großes start a gibt doppelblatt
+#großes nx staucht, großes ny streckt blatt
